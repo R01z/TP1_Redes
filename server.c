afbe0a6bf199482de17bbdc0a2f414f69a4c8a2a
R01z <igorrt66@gmail.com>
2022-10-25 21:30:13 -0300

    Update server.c

--- server.c
+++ server.c
@@ -23,15 +23,13 @@ struct sw racks[RACKS][SWITCHS];
 
 void execComando(const char* buf){
     int i = 0;
-    char comando[3]; //Obtem o comando
-    memset(comando, 0, 3);
+    char comando[BUFSZ]; //Obtem o comando
+    memset(comando, 0, BUFSZ);
 
     while(buf[i] != ' '){
         comando[i] = buf[i];
         i++;
     }
-
-    printf("[DEBUG] Comando obtido = %s\n", comando);
 
     if(strcmp(comando,"add") == 0){
         //Instalar 
